8e06554bffe4850e116d68d8fce5e17384912301
vegemil <euna501@gmail.com>
2015-03-13 20:57:16 +0900

    Cube 외관 완성 회전 코드 작성해야함

---
+++ Assets/Script/CubeStatus.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Status
+{
+	CENTER,
+	DIAGONAL,
+	EDGE,
+}
+
+public class CubeStatus : MonoBehaviour {
+
+	public Status status;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
